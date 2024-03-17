db1aeacf317ef18245bd84632dc9956db87e3a7a
Ilse GS <78421589+ilsegs@users.noreply.github.com>
2024-03-17 19:43:03 +0100

    neglecting styling rules in lint config

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
-import styles from '../styles';
-import { navVariants } from '../utils/motion';
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
 
 const Navbar = () => (
   <motion.nav
@@ -11,7 +11,7 @@ const Navbar = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <div className="absolute w-[50%] inset-0 gradient-01"/>
+    <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img
         src="/search.svg"
