d8032ec5535324417f9e74038a93c4c5f5e4c6c9
Ilse GS <78421589+ilsegs@users.noreply.github.com>
2024-03-17 19:37:00 +0100

    Update NewFeatures.jsx

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,4 +1,4 @@
-import styles from "../styles";
+import styles from '../styles';
 
 const NewFeatures = ({ imgUrl, title, subtitle }) => (
   <div className="flex-1 flex flex-col min-w-[210px] sm:max-w-[250px]">
@@ -8,7 +8,7 @@ const NewFeatures = ({ imgUrl, title, subtitle }) => (
       <img src={imgUrl} alt="icon" className="w-1/2 h-1/2 object-contain" />
     </div>
     <h1 className="mt-[26px] font-bold text-white text-[24px] leading-[30px]">
-      {" "}
+      {' '}
       {title}
     </h1>
     <p className="font-normal text-[18px] text-[#b0b0b0] flex-1 mt-4 leading-8">
